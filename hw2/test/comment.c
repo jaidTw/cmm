690133e2462ebd77d79d5a510c198abaeb0819a0
Jesse Huang <jessehuang2222@gmail.com>
2017-10-08 22:08:28 +0800

    add a comment test file

---
+++ hw2/test/comment.c
@@ -0,0 +1,13 @@
+int main()
+{
+	/* 1: this is just a sample */
+    /* test **/
+    code();
+    /*** test2 */
+
+	/***** 2: this is a 3-line
+	comment
+	********/
+	/* 3: this is /* /*the end ***/
+    return 0;
+}
