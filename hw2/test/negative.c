002e2b95acdc809e43fca395aba8e86cf9e23a02
Lasfolt <b03902071@ntu.edu.tw>
2017-10-15 16:26:06 +0800

    add signed constant

--- hw2/test/negative.c
+++ hw2/test/negative.c
@@ -5,9 +5,13 @@ int main()
     float h = 3.2;
     float i = 1.2e-60;
     float j = 5.6E+18;
-    float k = .0;
-    float l = 0.;
-	
+    float k = +.0;
+    float l = +0.;
+
+    int a = +1234;
+    int b = -1234;
+	int c = 1234;
+
 	float negf = -.3e5;
 	float negg = -05.9;
 	float negh = -3.2;
