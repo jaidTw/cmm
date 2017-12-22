9aa0246ea214101ccb5dc6148c18f6919f53d161
Jesse Huang <jessehuang2222@gmail.com>
2017-12-22 18:37:19 +0800

    update codegen

--- hw5/codegen.c
+++ hw5/codegen.c
@@ -5,72 +5,97 @@
 #include "header.h"
 #include "symbolTable.h"
 
-#define gencode(...) { fprintf(output, __VA_ARGS__); }
-#define genlabel(...) { fprintf(output, __VA_ARGS__); }
+#define GENCODE(fmt, ...) { fprintf(output, "\t" fmt "\n", ##__VA_ARGS__); }
+#define GENLABEL(name, ...) { fprintf(output, name ":\n" , ##__VA_ARGS__); }
+#define ALIGN(size) { fprintf(output, "\t.align %d\n", size); }
 
-#define ID_NAME(node) (node)->semantic_value.identifierSemanticValue.identifierName
-#define ID_KIND(node) (node)->semantic_value.identifierSemanticValue.kind
-#define CONST_TYPE(node) (node)->semantic_value.const1->const_type;
+#define CONST_TYPE(node) (node)->semantic_value.const1->const_type
 #define _CONST_OF_int(node) (node)->semantic_value.const1->const_u.intval
 #define _CONST_OF_float(node) (node)->semantic_value.const1->const_u.fval
-#define CONST_VAL_OF(node, type) _CONST_OF_##type(node)
-#define STMT_KIND(node) (node)->semantic_value.stmtSemanticValue.kind
+#define _CONST_OF_str(node) (node)->semantic_value.const1->const_u.sc
+#define CONST_VAL(node, type) _CONST_OF_##type(node)
+
+#define IS_LOCAL(node) (ENTRY(node)->nestingLevel > 0)
+#define NAME(node)  (node)->semantic_value.identifierSemanticValue.identifierName
+#define STMT(node)  (node)->semantic_value.stmtSemanticValue
+#define ID(node)    (node)->semantic_value.identifierSemanticValue
+#define ENTRY(node) (node)->semantic_value.identifierSemanticValue.symbolTableEntry
+
+#define FATAL(x) { \
+    fprintf(stderr, "Fatal : " x "\nAborted.\n"); \
+    exit(1); \
+}
+
+#define FOR_SIBLINGS(name, node) \
+    for(AST_NODE* (name) = (node) \
+        ; (name) \
+        ; (name) = (name)->rightSibling)
 
 static enum {NoneType, TextType, DataType} mode = NoneType;
 
 static FILE *output;
-static int _AR_offset; // count AR size
-static int _offset;
-static int _const = 0; // constant for constants and statments
+static int _AR_offset;
+static int _local_var_offset;
+static int _const; // constant for constants and statments
+static int regs[32];
+static int FPregs[32];
 
-static int regs[32] = {0};
-
-int allocReg(){
+int __allocReg_int(){
     // candidates: x9-x15, x19-x29
-    int i;
-    for(i = 9; i <= 29; i++){
+    for(int i = 9; i <= 29; i++) {
         if(i == 16) i = 19;
-        if(!regs[i]) return (regs[i] = 1);
+        if(!regs[i]) {
+            regs[i] = 1;
+            return i;
+        }
     }
-    printf("Out of registers\n");
-    exit(1);
+    return -1;
 }
 
-static __inline__ void freeReg(int reg){
-    regs[reg] = 0;
+int __allocReg_float(){
+    for(int i = 16; i <= 31; i++)
+        if(!FPregs[i]) {
+            FPregs[i] = 1;
+            return i;
+        }
+    return -1;
 }
+
+#define allocReg(type) __allocReg_##type()
+
+static __inline__ void __freeReg_int(int reg) { regs[reg] = 0 ; }
+static __inline__ void __freeReg_float(int reg) {FPregs[reg] = 0; }
+#define freeReg(reg, type) __freeReg_##type(reg)
 
 static __inline__ void switchToText() {
-    if(mode != TextType)
-        genlabel(".text\n");
+    if(mode != TextType) fprintf(output, ".text\n");
 }
+
 static __inline__ void switchToData() {
-    if(mode != DataType)
-        genlabel(".data\n");
+    if(mode != DataType) fprintf(output, ".data\n");
 }
 
-static __inline__ void genRead(int offset) {
-    gencode("\tbl _read_int\n"
-            "\tmov w9, w0\n" /* maybe we should allocReg instead of fixed w9 */
-            "\tstr w9, [x29, #-%d]\n", offset);
+static __inline__ void genRead() {
+    GENCODE("bl _read_int");
 }
 
-static __inline__ void genFread(int offset) {
-    gencode("\tbl _read_float\n"
-            "\tfmov s16, s0\n"
-            "\tstr w9, [x29, #-%d]\n", offset);
+static __inline__ void genFread() {
+    GENCODE("bl _read_float");
 }
 
-static __inline__ void genWriteInt(int offset) {
-    gencode("\tldr w9, [x29, #-%d]\n"
-            "\tmov w0, w9\n"
-            "\tbl _write_int\n", offset);
+static __inline__ void genWriteInt(int reg) {
+    GENCODE("mov w0, w%d", reg);
+    GENCODE("bl _write_int");
 }
 
-static __inline__ void genWriteFloat(int offset) {
-    gencode("ldr s16, [x29, #-%d]"
-            "mov s0, s16"
-            "bl _write_float", offset);
+static __inline__ void genWriteFloat(int reg) {
+    GENCODE("fmov s0, s%d", reg);
+    GENCODE("bl _write_float");
+}
+
+static __inline__ void genWriteStr(int reg) {
+    GENCODE("mov x0, x%d", reg);
+    GENCODE("bl _write_str");
 }
 
 void genPrologue(char *name);
@@ -99,7 +124,6 @@ void genGeneralNode(AST_NODE *node);
 
 void genGeneralNode(AST_NODE *node)
 {
-    AST_NODE *child= node->child;
     switch(node->nodeType){
         case VARIABLE_DECL_LIST_NODE:
             genLocalDeclarations(node);
@@ -108,16 +132,12 @@ void genGeneralNode(AST_NODE *node)
             genStatementList(node);
             break;
         case NONEMPTY_ASSIGN_EXPR_LIST_NODE:
-            while(child){
+            FOR_SIBLINGS(child, node->child)
                 genAssignOrExpr(child);
-                child = child->rightSibling;
-            }
             break;
         case NONEMPTY_RELOP_EXPR_LIST_NODE:
-            while(child){
+            FOR_SIBLINGS(child, node->child)
                 genExprRelatedNode(child);
-                child= child->rightSibling;
-            }
             break;
         case NUL_NODE:
         default:
@@ -126,58 +146,51 @@ void genGeneralNode(AST_NODE *node)
 }
 
 void genLocalDeclarations(AST_NODE* node) {
-    for(AST_NODE *decl_node = node->child
-        ; decl_node
-        ; decl_node = decl_node->rightSibling) {
+    FOR_SIBLINGS(decl_node, node->child) {
         AST_NODE *type_node = decl_node->child;
         /* TODO: type specific initialization */
-        if(type_node->dataType == INT_TYPE) {
-            _offset += 4;
-            /* insert into symbol table ? */
-        } else if(type_node->dataType == FLOAT_TYPE) {
-        }
-        if(ID_KIND(type_node->rightSibling) == WITH_INIT_ID) {
+        SymbolTableEntry *entry = ENTRY(type_node->rightSibling);
+        _local_var_offset += 4;
+        entry->offset = _local_var_offset;
+        if(ID(type_node->rightSibling).kind == WITH_INIT_ID) {
             /* initialize */
-            AST_NODE *val_node = type_node->rightSibling->child;
-            if(type_node->dataType == INT_TYPE) {
-                int reg = allocReg();
-                node->place = reg;
-                gencode("\tmov x%d, #%d\n", reg, CONST_VAL_OF(val_node, int));
-                gencode("\tstr x%d, [x29, #-%d]\n", reg, _offset);
-            } else if(type_node->dataType == FLOAT_TYPE) {
-                /* TODO generate constant data and label*/
-            }
+            AST_NODE *relop = type_node->rightSibling->child;
+            genExprRelatedNode(relop);
+            GENCODE("str %c%d, [x29, #-%d]",
+                type_node->dataType == INT_TYPE ? 'w' : 's',
+                relop->place, entry->offset);
+            type_node->dataType == INT_TYPE ?
+                freeReg(relop->place, int) : freeReg(relop->place, float);
         }
     }
 }
 
-void genExprRelatedNode(AST_NODE *node){
+void genExprRelatedNode(AST_NODE *node) {
     switch(node->nodeType){
         case EXPR_NODE:
             //genExprNode(node);
             break;
         case STMT_NODE:
-            // function call
-            //genFunctionCall(node);
+            genFunctionCall(node);
             break;
         case IDENTIFIER_NODE:
-            //genVariableRvalue(node);
+            genVariableRvalue(node);
             break;
         case CONST_VALUE_NODE:
-            //genConstValueNode(node);
+            genConstValueNode(node);
             break;
         default:
             break;
     }
 }
 
-void genAssignOrExpr(AST_NODE *node){
+void genAssignOrExpr(AST_NODE *node) {
     if(node->nodeType == STMT_NODE){
-        if(STMT_KIND(node) == ASSIGN_STMT){
+        if(STMT(node).kind == ASSIGN_STMT){
             //genAssignmentStmt(node);
         }
-        else if(STMT_KIND(node) == FUNCTION_CALL_STMT){
-            //genFunctionCall(node);
+        else if(STMT(node).kind == FUNCTION_CALL_STMT){
+            genFunctionCall(node);
         }
     }
     else{
@@ -185,11 +198,28 @@ void genAssignOrExpr(AST_NODE *node){
     }
 }
 
+void genFunctionCall(AST_NODE *node) {
+    char *func_name = NAME(node->child);
+    AST_NODE *relop_expr = node->child->rightSibling;
+    genExprRelatedNode(relop_expr->child);
+    relop_expr->place = relop_expr->child->place;
+    if(!strcmp(func_name, "write")) {
+        if(relop_expr->child->dataType == INT_TYPE)
+            genWriteInt(relop_expr->place);
+        else if(relop_expr->child->dataType == FLOAT_TYPE)
+            genWriteFloat(relop_expr->place);
+        else if(relop_expr->child->dataType == CONST_STRING_TYPE)
+            genWriteStr(relop_expr->place);
+    }
+    else if(!strcmp(func_name, "read"))
+        ;
+    else if(!strcmp(func_name, "fread"))
+        ;
+}
+
 
 void genBlockNode(AST_NODE *block_node){
-    for(AST_NODE *child = block_node->child
-        ; child
-        ; child = child->rightSibling)
+    FOR_SIBLINGS(child, block_node->child)
         genGeneralNode(child);
 }
 
@@ -201,7 +231,7 @@ void genStmtNode(AST_NODE *stmt_node){
         genBlockNode(stmt_node);
     }
     else{
-        switch(STMT_KIND(stmt_node)){
+        switch(STMT(stmt_node).kind){
             case WHILE_STMT:
                 //genWhileStmt(stmt_node);
                 break;
@@ -215,7 +245,7 @@ void genStmtNode(AST_NODE *stmt_node){
                 //genIfStmt(stmt_node);
                 break;
             case FUNCTION_CALL_STMT:
-                //genFunctionCall(stmt_node);
+                genFunctionCall(stmt_node);
                 break;
             case RETURN_STMT:
                 //genReturnStmt(stmt_node);
@@ -224,11 +254,9 @@ void genStmtNode(AST_NODE *stmt_node){
     }
 }
 
-void genStatementList(AST_NODE* stmt_list_node){
-    for(AST_NODE *stmt_list = stmt_list_node->child
-        ; stmt_list
-        ; stmt_list = stmt_list->rightSibling)
-        genStmtNode(stmt_list);
+void genStatementList(AST_NODE* node){
+    FOR_SIBLINGS(stmt, node->child)
+        genStmtNode(stmt);
 }
 
 void genGlobalDeclration(AST_NODE *decl){
@@ -276,7 +304,7 @@ void genGlobalDeclration(AST_NODE *decl){
         /* copy-past end */
         if(decl->semantic_value.declSemanticValue.kind == VARIABLE_DECL){
             if(attribute->attr.typeDescriptor->kind == SCALAR_TYPE_DESCRIPTOR){
-                genlabel("_%s: .word 0\n", ID_NAME(id_list));
+                fprintf(output, "_%s: .word 0\n", NAME(id_list));
             }
             else{
                 int array_size = 4;
@@ -285,7 +313,7 @@ void genGlobalDeclration(AST_NODE *decl){
                 for(int i = 0; i < array_dim; ++i){
                    array_size *= array_dims[i];
                 }
-                genlabel("_%s: .skip %d\n", ID_NAME(id_list), array_size);
+               fprintf(output, "_%s: .skip %d\n", NAME(id_list), array_size);
             }
         }
         attribute->global = 1;
@@ -293,17 +321,55 @@ void genGlobalDeclration(AST_NODE *decl){
     }
 }
 
-void genGlobalDeclrations(AST_NODE *decl_list_node){
-    for(AST_NODE *decl = decl_list_node->child
-        ; decl
-        ; decl = decl->rightSibling)
+void genVariableRvalue(AST_NODE *id_node) {
+    if(id_node->dataType == INT_TYPE)
+        id_node->place = allocReg(int);
+    else if(id_node->dataType == FLOAT_TYPE)
+        id_node->place = allocReg(float);
+    else
+        FATAL("invalid type");
+
+    /* localor global */
+    if(IS_LOCAL(id_node)) {
+        /* TODO: array */
+        GENCODE("ldr %c%d, [x29, #-%d]",
+            id_node->dataType == INT_TYPE ? 'w' : 's',
+            id_node->place, ENTRY(id_node)->offset);
+    } else {
+        /* TODO: array */
+        GENCODE("ldr %c%d, =_%s",
+            id_node->dataType == INT_TYPE ? 'w' : 's',
+            id_node->place, NAME(id_node));
+    }
+}
+
+void genConstValueNode(AST_NODE *node) {
+    if(CONST_TYPE(node) == INTEGERC) {
+        node->place = allocReg(int);
+        GENCODE("mov w%d, #%d", node->place, CONST_VAL(node, int));
+    } else if(CONST_TYPE(node) == FLOATC) {
+        node->place = allocReg(float);
+        switchToData();
+        GENCODE("_CONST_%d: .float %f", ++_const, CONST_VAL(node, float));
+        switchToText();
+        GENCODE("ldr s%d, _CONST_%d", node->place, _const);
+    } else {
+        node->place = allocReg(int);
+        switchToData();
+        GENCODE("_CONST_%d: .string %s", ++_const, CONST_VAL(node, str));
+        ALIGN(4);
+        switchToText();
+        GENCODE("ldr x%d, =_CONST_%d", node->place, _const);
+    }
+}
+
+void genGlobalDeclrations(AST_NODE *node){
+    FOR_SIBLINGS(decl, node->child)
         genGlobalDeclration(decl);
 }
 
 void genProgramNode(AST_NODE *program_node){
-    for(AST_NODE *global_decl_list = program_node->child
-        ; global_decl_list
-        ; global_decl_list = global_decl_list->rightSibling) {
+    FOR_SIBLINGS(global_decl_list, program_node->child) {
         if(global_decl_list->nodeType == VARIABLE_DECL_LIST_NODE) 
             genGlobalDeclrations(global_decl_list);
         else if(global_decl_list->nodeType == DECLARATION_NODE)
@@ -312,14 +378,13 @@ void genProgramNode(AST_NODE *program_node){
 }
 
 void genFunctionDeclration(AST_NODE *func_decl){
-    char *name = ID_NAME(func_decl->child->rightSibling);
+    _AR_offset = 0;
+    char *name = NAME(func_decl->child->rightSibling);
     AST_NODE *paramListNode = func_decl->child->rightSibling->rightSibling;
     genPrologue(name);
 
-    _offset = 0;
-    for(AST_NODE *list_node = paramListNode->rightSibling->child
-        ; list_node
-        ; list_node = list_node->rightSibling)
+    _local_var_offset = 0;
+    FOR_SIBLINGS(list_node, paramListNode->rightSibling->child)
         genGeneralNode(list_node);
 
     genEpilogue(name);
@@ -331,44 +396,43 @@ void codeGeneration(AST_NODE *root) {
     fclose(output);
 }
 
-void genEpilogue(char *name){
-    genlabel("_end_%s:\n", name);
-
-    int i, offset = 8;
-    for(i = 9; i <= 15; i++, offset += 8)
-        gencode("\tldr x%d, [sp, #%d]\n", i, offset);
-    for(i = 19; i <= 29; i++, offset += 8)
-        gencode("\tldr x%d, [sp, #%d]\n", i, offset);
-
-    gencode("\tldr x30, [x29, #8]\n"
-            "\tadd sp, sp, #8\n"
-            "\tldr x29, [x29, #0]\n"
-            "\tret x30\n");
-    switchToData();
-    gencode("\t_frameSize_%s: .word %d\n", name, _AR_offset);
-    switchToText();
-}
-
 void genPrologue(char *name){
     switchToText();
-    genlabel("_start_%s:\n", name);
-    gencode("\tstr x30, [sp, #0]\n"
-            "\tstr x29, [sp, #-8]\n"
-            "\tadd x29, sp, #-8\n"
-            "\tadd sp, sp, #-16\n"
-            "\tldr x30, =_frameSize_%s\n"
-            "\tldr w30, [x30, #0]\n"
-            "\tsub sp, sp, w30\n", name);
-    
-    int offset = 8;
-    int i;
-    // caller saved registers
-    for(i = 9; i <= 15; i++, offset += 8)
-        gencode("\tstr x%d, [sp, #%d]\n", i, offset);
-    // callee saved registers
-    for(i = 19; i <= 29; i++, offset += 8)
-        gencode("\tstr x%d, [sp, #%d]\n", i, offset);
+    GENLABEL("_start_%s", name);
+    GENCODE("str x30, [sp, #0]");
+    GENCODE("str x29, [sp, #-8]");
+    GENCODE("add x29, sp, #-8");
+    GENCODE("add sp, sp, #-16");
+    GENCODE("ldr x30, =_frameSize_%s", name);
+    GENCODE("ldr w30, [x30, #0]");
+    GENCODE("sub sp, sp, w30");
 
-    _AR_offset = offset - 4;
-    genlabel("_begin_%s:\n", name);
+    int offset = 8;
+    // push callee saved registers
+    for(int i = 19; i <= 29; i++, offset += 8)
+        GENCODE("str x%d, [sp, #%d]", i, offset);
+    // VFP regs
+    for(int i = 16; i <= 23; i++, offset += 8)
+        GENCODE("ldr s%d, [sp, #%d]", i, offset);
+
+    _AR_offset = offset;
+    GENLABEL("_begin_%s", name);
+}
+
+void genEpilogue(char *name){
+    GENLABEL("_end_%s", name);
+
+    int offset = 8;
+    for(int i = 19; i <= 29; i++, offset += 8)
+        GENCODE("ldr x%d, [sp, #%d]", i, offset);
+    for(int i = 16; i <= 23; i++, offset += 8)
+        GENCODE("ldr s%d, [sp, #%d]", i, offset);
+
+    GENCODE("ldr x30, [x29, #8]")
+    GENCODE("add sp, x29, #8")
+    GENCODE("ldr x29, [x29, #0]")
+    GENCODE("ret x30");
+    switchToData();
+    fprintf(output, "_frameSize_%s: .word %d\n", name, _AR_offset);
+    switchToText();
 }
