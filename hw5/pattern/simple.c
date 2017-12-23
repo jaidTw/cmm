b6ed03975a0e5d7c2921bfd81bb19a66b6b3ce12
Jesse Huang <jessehuang2222@gmail.com>
2017-12-24 03:55:30 +0800

    update testcases

--- hw5/pattern/simple.c
+++ hw5/pattern/simple.c
@@ -1,8 +1,33 @@
+int func(int a, int b, int c) {
+    write(a);
+    write("\n");
+    write(b);
+    write("\n");
+    write(c);
+    a = 5;
+    return a;
+}
+
+int func2(float a, float b, float c) {
+    write(a);
+    write("\n");
+    write(b);
+    write("\n");
+    write(c);
+    a = 100.0;
+    return a;
+}
 int MAIN(){
-    int a = 5;
-    float b = 3.0;
-    while((b < 10.0)) {
-        write(b);
-        b = b + 1.1;
-    }
+    int a = 100;
+    int b = 200;
+    float aa = 1.0;
+    func(a * 2, b * 3, 300);
+    write("\n");
+    aa = func2(a * 2.0, b * 3.0, 300);
+    write("\n");
+    write(a);
+    write("\n");
+    write(aa);
+    write("\n");
+    return 0;
 }
